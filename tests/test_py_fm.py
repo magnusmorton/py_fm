bd42e73d4d0e11c9529a811425506fc442f74a69
Magnus Morton <magnusmorton@gmail.com>
2012-02-29 00:51:06 +0000

    added tests for to_list

--- tests/test_py_fm.py
+++ tests/test_py_fm.py
@@ -8,8 +8,13 @@ class TestSystem(unittest.TestCase):
                     [1,    3,   -2,   -5],
                     [1, -1/1,   -3,  8/2],
                     [1,    2,    0,   -3]]
+    BANERJEE2   =    [[1,  -2,   11,   3],
+                    [1,    3,   -2, -5],
+                    [1,   -1,   -3,    4],
+                    [1,    4,    0,   -6]]
+    
     def setUp(self):
-        self.system = System(BOOK_EXAMPLE)
+        self.system = System(TestSystem.BOOK_EXAMPLE)
     
     def test_can_create(self):
         assert self.system != None
@@ -19,7 +24,13 @@ class TestSystem(unittest.TestCase):
         sol = self.system.solve()
         sol.dump()
         
-    def test_to_list(self):
-        assert self.system.to_list() == BOOK_EXAMPLE, "array not recovered"
+    def test_to_list_book(self):
+        assert self.system.to_list() == TestSystem.BOOK_EXAMPLE, "array not recovered"
+      
+    def test_to_list_banerjee2(self):
+        system2 = System(TestSystem.BANERJEE2)
+        assert system2.to_list() == TestSystem.BANERJEE2, "array not recovered correctly"
+        
+    
         
     
