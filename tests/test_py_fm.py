22f35c434f30fbef526bd61a2f559cdbf88a1ddd
Magnus Morton <magnusmorton@gmail.com>
2012-02-29 00:23:29 +0000

    added further tests/drivers

--- tests/test_py_fm.py
+++ tests/test_py_fm.py
@@ -3,14 +3,23 @@ import tests
 from py_fm import *
 
 class TestSystem(unittest.TestCase):
+    """this is not a true unit test set, more a driver to check that it works"""
+    BOOK_EXAMPLE = [[1,   -2,   11,    3],
+                    [1,    3,   -2,   -5],
+                    [1, -1/1,   -3,  8/2],
+                    [1,    2,    0,   -3]]
     def setUp(self):
-        self.system = System([[11,   -2,   11,    3],
-                        [1,    3,   -2,   -5],
-                        [1, -1/1,   -3,  8/2],
-                        [1,    2,    0,   -3]])
+        self.system = System(BOOK_EXAMPLE)
     
     def test_can_create(self):
         assert self.system != None
         
     def test_solve(self):
-        self.system.solve()
+        self.system.dump()
+        sol = self.system.solve()
+        sol.dump()
+        
+    def test_to_list(self):
+        assert self.system.to_list() == BOOK_EXAMPLE, "array not recovered"
+        
+    
